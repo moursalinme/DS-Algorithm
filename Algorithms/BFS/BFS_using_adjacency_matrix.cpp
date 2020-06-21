fcd5a11614fa3b140bac56fa6becb3b1a866d72a
Moursalin Emon <40349059+moursalinme@users.noreply.github.com>
2020-06-21 14:37:19 +0600

    Update BFS_using_adjacency_matrix.cpp

--- Algorithms/BFS/BFS_using_adjacency_matrix.cpp
+++ Algorithms/BFS/BFS_using_adjacency_matrix.cpp
@@ -22,8 +22,8 @@ void bfs(bool adj_list[][total_nodes], int dis_from_src[], int source) {
 		
 		for (int i = 0; i < total_nodes; ++i) {
 			if(adj_list[now_at][i] && vis[i] == false  && i!= now_at) {  /* if i has and edge with current node(now_at) 
-																			, i is not the current node and
-																			the node(i) is not visited yet */
+								                                          , i is not the current node and
+                              						                       the node(i) is not visited yet */
 				q.push(i);
 				dis_from_src[i] = dis_from_src[now_at] + 1;       /* distacne of current node from the source node
 																	 = distance of my current node from the source node + 1 */
