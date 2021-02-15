3900cb52ed0bef667ce550747febc6be68e94a86
moursalinme <me.moursalin@gmail.com>
2021-02-15 18:12:48 +0600

    implementation by array (using DMA)

--- Data Structure/Queue/Array Implementation of Queue/queue.cpp
+++ Data Structure/Queue/Array Implementation of Queue/queue.cpp
@@ -1,54 +1,54 @@
 template <class T> 
 class Queue {
-	int n, siz, head, tail;
-	T *arr = NULL;
-	
+    int n, siz, head, tail;
+    T *arr = NULL;
+    
 public :
-	Queue () = default;
+    Queue () = default;
 
-	Queue (int t)  {
-		arr = new T[t];
-		n = t;
-		siz = head = tail = 0;
-	}
+    Queue (int t)  {
+        arr = new T[t];
+        n = t;
+        siz = head = tail = 0;
+    }
 
-	void set_size (int t) {
-		n = t;
-		head = tail = siz = 0;
-		
-		if (arr != NULL) 
-			free (arr);
-		arr = new T[t];
-	}
+    void set_size (int t) {
+        n = t;
+        head = tail = siz = 0;
+        
+        if (arr != NULL) 
+            free (arr);
+        arr = new T[t];
+    }
 
-	int size () {
-		return siz;
-	}
+    int size () {
+        return siz;
+    }
 
-	void enqueue (T data) {
-		if (siz == n) 
-			cout << "Overflow\n";
-		else {
-			arr[tail] = data;
-			tail = (tail + 1) % n;
-			++siz;
-		}
-	}
+    void enqueue (T data) {
+        if (siz == n) 
+            cout << "Overflow\n";
+        else {
+            arr[tail] = data;
+            tail = (tail + 1) % n;
+            ++siz;
+        }
+    }
 
-	void dequeue() {
-		if (siz) {
-			head = (head + 1) % n;
-			--siz;
-		}
-		else cout << "Underflow\n";
-	}
+    void dequeue() {
+        if (siz) {
+            head = (head + 1) % n;
+            --siz;
+        }
+        else cout << "Underflow\n";
+    }
 
-	T front () {
-		assert (siz);
-		return arr[head];
-	}
+    T front () {
+        assert (siz);
+        return arr[head];
+    }
 
-	~Queue () {
-		free (arr);
-	}
+    ~Queue () {
+        free (arr);
+    }
 };
